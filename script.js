50d08b10b74a6712f09c88543baa182c6134deb9
mgkinvitation <solis.ss0922@gmail.com>
2024-10-12 09:39:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function copyToClipboard() {
-    const number = document.querySelector('.no-rek').textContent.split(' - ')[0]; // Ambil hanya nomor rekening
+function copyToClipboard(button) {
+    const listItem = button.closest('.list-group-item'); // Temukan elemen terdekat dengan kelas .list-group-item
+    const number = listItem.querySelector('.no-rek').textContent.split(' - ')[0]; // Ambil hanya nomor rekening
     navigator.clipboard.writeText(number).then(() => {
         alert("Nomor rekening telah disalin: " + number);
     }).catch(err => {
